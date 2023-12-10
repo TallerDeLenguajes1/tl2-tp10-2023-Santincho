f010fd47ea41d8368011bc394e41320779b96fb9
Santiago Gonzalez <santiriver2009@gmail.com>
2023-12-10 16:16:41 -0300

    quiequqw

--- Program.cs
+++ Program.cs
@@ -3,6 +3,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(100);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
